9846a4b329e570a910dc8ea6fedeb28e81f7a89d
Mika Hänninen <mika@robocorp.com>
2022-01-30 21:23:05 +0200

    Update ExtendedSelenium.py

--- ExtendedSelenium.py
+++ ExtendedSelenium.py
@@ -1,12 +1,32 @@
 from RPA.Browser.Selenium import Selenium
+from webdrivermanager import ChromeDriverManager
 from SeleniumLibrary.base import keyword
 
 class ExtendedSelenium(Selenium):
 
     def __init__(self, *args, **kwargs):
         Selenium.__init__(self, *args, **kwargs)
-
+        cdm = ChromeDriverManager(link_path="AUTO")
+        cdm.download_and_install()
+        
     @keyword
     def looking_at_element(self, locator):
         element = self.get_webelement(locator)
         self.logger.warn(dir(element))
+
+    @keyword
+    def open_site(self, url, **kwargs):
+        desired_capabilities = {
+            "goog:loggingPrefs" : { 'browser':'ALL',  'driver': 'ALL', 'performance': 'ALL' }
+        }
+        self.open_browser(
+            url=url,
+            desired_capabilities=desired_capabilities,
+            **kwargs
+        )
+
+    @keyword
+    def print_webdriver_log(self, logtype):
+        print(f"\n{logtype.capitalize()} Log")
+        return self.driver.get_log(logtype)
+        
